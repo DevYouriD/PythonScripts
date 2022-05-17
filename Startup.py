fda99ce67578d9fde47f3160062b3c561359d322
DevYouriD <youri.dera@gmail.com>
2022-05-17 21:57:09 +0200

    Add files via upload

---
+++ Startup.py
@@ -0,0 +1,5 @@
+import os
+os.startfile('C:\\Users\\youri\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Teams')
+os.startfile('C:\\Users\\youri\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Spotify')
+os.startfile('C:\\Users\\youri\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\Programs\\Visual Studio Code\\Visual Studio Code')
+os.startfile('')
